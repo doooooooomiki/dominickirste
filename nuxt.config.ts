ee013015f01ebb400d3b8f6f21e23b7cd3e81207
Dominic Kirste <dominic.kirste@deutschebahn.com>
2026-03-03 21:43:51 +0100

    create nuxt@latest

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxt/eslint'],
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'hi.',
+      htmlAttrs: {
+        lang: 'de',
+      },
+    },
+  },
+  compatibilityDate: '2025-07-15',
+  eslint: {
+    config: {
+      stylistic: {
+        semi: false,
+        quotes: 'single',
+        commaDangle: 'always-multiline',
+        indent: 2,
+        arrowParens: true,
+        braceStyle: '1tbs',
+      },
+    },
+  },
+})
