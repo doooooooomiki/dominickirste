9f5ad329efe3d395f8cc2e74c871c5f402ec008b
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2026-03-12 10:54:00 +0100

    emojify

--- app/components/MotivationLetterSection.vue
+++ app/components/MotivationLetterSection.vue
@@ -39,6 +39,24 @@ onUnmounted(() => ctx.revert())
     class="introduction"
   >
     <div
+      data-speed="clamp(0.92)"
+      class="emoji emoji--happy-face"
+    >
+      <img src="/emoji--happy-face.png">
+    </div>
+    <div
+      data-speed="clamp(0.91)"
+      class="emoji emoji--cool-face"
+    >
+      <img src="/emoji--cool-face.png">
+    </div>
+    <div
+      data-speed="clamp(0.9)"
+      class="emoji emoji--heart-face"
+    >
+      <img src="/emoji--heart-face.png">
+    </div>
+    <div
       ref="hi"
       class="layout-center"
     >
@@ -49,3 +67,35 @@ onUnmounted(() => ctx.revert())
     </div>
   </section>
 </template>
+
+<style>
+.introduction {
+  position: relative;
+}
+
+.emoji {
+  position: absolute;
+  width: var(--step-5);
+  height: var(--step-5);
+}
+
+.emoji--happy-face {
+  right: 1%;
+  bottom: 24%;
+}
+
+.emoji--cool-face {
+  right: 4%;
+  bottom: 8%;
+}
+
+.emoji--heart-face {
+  right: 2%;
+  bottom: -8%;
+}
+
+.emoji img {
+  width: 100%;
+  height: 100%;
+}
+</style>
