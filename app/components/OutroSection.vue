0ef53479668ff735f5785255ef7453164d7ed956
doooooooomiki <dominic.kirste@hyperlinkdrifter.com>
2026-03-18 01:17:30 +0100

    nothing else matter js

--- app/components/OutroSection.vue
+++ app/components/OutroSection.vue
@@ -1,9 +1,183 @@
 <script setup lang="ts">
 import { gsap } from 'gsap'
+import Matter from 'matter-js'
+import { useResizeObserver, useElementBounding } from '@vueuse/core'
 
 const outro = useTemplateRef('outro')
 const outroinner = useTemplateRef('outro-inner')
+const mattercontainer = useTemplateRef('matter-container')
+
+const { width, height } = useElementBounding(mattercontainer)
+
 let ctx: gsap.Context
+
+let engine: Matter.Engine
+let render: Matter.Render
+let runner: Matter.Runner
+let ground: Matter.Body
+let wallLeft: Matter.Body
+let wallRight: Matter.Body
+
+const setupMatter = () => {
+  if (!mattercontainer.value) return
+  const Engine = Matter.Engine
+  const Render = Matter.Render
+  const Runner = Matter.Runner
+  const Bodies = Matter.Bodies
+  const Composite = Matter.Composite
+
+  engine = Engine.create()
+
+  render = Render.create({
+    element: mattercontainer.value,
+    engine: engine,
+    options: {
+      wireframes: false,
+      background: 'transparent',
+      width: width.value,
+      height: height.value,
+      pixelRatio: window.devicePixelRatio,
+    },
+  })
+
+  ground = Bodies.rectangle(width.value * 0.5, height.value, width.value, 60, { isStatic: true, render: {
+    fillStyle: 'transparent',
+  } })
+  wallLeft = Bodies.rectangle(0, height.value * 0.5, 60, height.value, { isStatic: true, render: {
+    fillStyle: 'transparent',
+  } })
+  wallRight = Bodies.rectangle(width.value, height.value * 0.5, 60, height.value, { isStatic: true, render: {
+    fillStyle: 'transparent',
+  } })
+
+  const randomX = gsap.utils.random(80, width.value - 80, 4, true)
+  const randomY = gsap.utils.random(-240, -24, 2, true)
+
+  const options = {
+    friction: 0.00001, restitution: 0.5, density: 0.001,
+
+  }
+
+  const emojiCoolFace = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--cool-face.png',
+      },
+    },
+  })
+
+  const emojiHappyFace = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--happy-face.png',
+      },
+    },
+  })
+
+  const emojiHappyStar = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--happy-star.png',
+      },
+    },
+  })
+
+  const emojiHeartFace = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--heart-face.png',
+      },
+    },
+  })
+
+  const emojiHeartsOnFace = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--hearts-on-face.png',
+      },
+    },
+  })
+
+  const emojiSatisfiedFace = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--satisfied-face.png',
+      },
+    },
+  })
+
+  const emojiSmilingFace = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--smiling-face.png',
+      },
+    },
+  })
+
+  const emojiStarFace = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--star-face.png',
+      },
+    },
+  })
+
+  const emojiSurprisedFace = Bodies.circle(randomX(), randomY(), 64, {
+    ...options,
+    render: {
+      sprite: {
+        xScale: 120 / 512,
+        yScale: 120 / 512,
+        texture: '/emoji--surprised-face.png',
+      },
+    },
+  })
+
+  Composite.add(engine.world, [
+    ground,
+    wallLeft,
+    wallRight,
+    emojiCoolFace,
+    emojiHappyFace,
+    emojiHappyStar,
+    emojiHeartFace,
+    emojiHeartsOnFace,
+    emojiSatisfiedFace,
+    emojiSmilingFace,
+    emojiStarFace,
+    emojiSurprisedFace,
+  ])
+
+  Render.run(render)
+
+  runner = Runner.create()
+
+  Runner.run(runner, engine)
+}
 
 const setupGsap = () => {
   if (!outro.value || !outroinner.value) return
@@ -25,9 +199,24 @@ const setupGsap = () => {
         {
           ease: 'none',
           yPercent: 0,
+          onComplete: () => setupMatter(),
         })
   }, outro.value)
 }
+
+useResizeObserver(mattercontainer, () => {
+  if (!mattercontainer.value || !render) return
+  render.canvas.width = width.value
+  render.canvas.height = height.value
+  Matter.Composite.remove(engine.world, ground)
+  ground = Matter.Bodies.rectangle(width.value * 0.5, height.value, width.value, 60, { isStatic: true, render: {
+    fillStyle: 'transparent',
+  } })
+  Matter.Composite.add(engine.world, [ground])
+  Matter.Body.setPosition(ground, { x: width.value * 0.5, y: height.value })
+  Matter.Body.setPosition(wallLeft, { x: 16, y: height.value * 0.5 })
+  Matter.Body.setPosition(wallRight, { x: width.value - 16, y: height.value * 0.5 })
+})
 
 onMounted(() => setupGsap())
 
@@ -37,7 +226,8 @@ onUnmounted(() => ctx.revert())
 <template>
   <footer ref="outro" class="outro layout-stack">
     <div ref="outro-inner" class="outro-inner layout-stack-block">
-      <div class="layout-center">
+      <div ref="matter-container" class="matter-container layout-center" />
+      <div class="outro-content layout-center layout-cover">
         <NameLine as="h2" dir="up" color="soy-sauce" />
       </div>
     </div>
@@ -52,16 +242,22 @@ onUnmounted(() => ctx.revert())
   block-size: 100svh;
 }
 
+.matter-container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  inset: 0;
+}
+
 .outro-inner {
   background-color: inherit;
   position: inherit;
-  z-index: inherit;
   block-size: inherit;
   display: flex;
   flex-direction: column;
 }
 
-.outro-inner > div {
-  margin-block-start: auto;
+.outro-content {
+  position: relative
 }
 </style>
